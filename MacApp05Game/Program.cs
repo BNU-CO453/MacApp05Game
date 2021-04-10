cfd1303c0dfa9018d5a5cfa9ef99e75027ab499d
Derek Peacock <derek.peacock@yahoo.com>
2021-04-10 16:49:24 +0100

    Added all the template code and content

--- MacApp05Game/Program.cs
+++ MacApp05Game/Program.cs
@@ -7,7 +7,7 @@ namespace MacApp05Game
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new App05Game())
                 game.Run();
         }
     }
